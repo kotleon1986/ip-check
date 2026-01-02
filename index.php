ae1ae3650341a63db38df126c8c12931ff2d4b2c
Leonid Kotanidi <kotleon1986@gmail.com>
2026-01-02 20:52:41 +0400

    added proper ip address

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
+if (array_key_exists('do-connecting-ip', $_SERVER)) {
+	$key = 'do-connecting-ip';
+} else if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
 	$key = 'HTTP_X_FORWARDED_FOR';
 } else {
 	$key = 'REMOTE_ADDR';	
